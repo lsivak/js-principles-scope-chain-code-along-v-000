8027b2d8581d598b201a6ff262535d555f7fa898
Lara Sanders <larasanders@gmail.com>
2018-05-18 16:59:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
+const leastFavoriteCustomer = 'rob'
 
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase()
 }
 
 function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
+  leastFavoriteCustomer = 'rob'
 }
 
 
